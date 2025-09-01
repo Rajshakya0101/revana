78a6a59521ca50974fba7a243853692f562a7698
Raj Shakya <rs7267887611@gmail.com>
2025-09-01 23:26:24 +0530

    downgraded to react 16

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// AFTER (React 16)
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+  document.getElementById("root")
+);
